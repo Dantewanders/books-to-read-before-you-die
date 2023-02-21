116f73e98c38d8cf28d8cf1f9bb604c87339c0a4
dante taylor <dantewanders@gmail.com>
2023-02-20 22:26:06 -0500

    updates and fix to search css

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+// import { useGlobalContext } from '../../context.';
+import "./SearchForm.css";
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchForm = (props) => {
   return (
-    <div>SearchForm</div>
+    <div className= 'search-form'>
+      <div className= 'container'>
+        <div className= 'search-form-content'>
+          <form className= 'search-form'></form>
+          <div className= 'search-form-elements flex flex-c bg-white'>
+            <input type = "text"
+            className ='form-control' placeholder = '  The Inferno ...'/>
+            <button type = "submit" className = 'flex flex-c'>
+              <SearchIcon sx={{ fontSize: 60 }}className = 'text-light-blue'/>
+              {/* icon from material ui and instructions */}
+            </button>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
+
 
 export default SearchForm
 
