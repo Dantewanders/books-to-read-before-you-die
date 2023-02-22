ef527444a166df622b0885e011de5b8b70872189
dante taylor <dantewanders@gmail.com>
2023-02-22 11:43:30 -0500

    Search filter built

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React ,{useRef,useEffect}from 'react'
 import { useNavigate } from 'react-router-dom';
-// import { useGlobalContext } from '../../context.';
+import { useContext } from '../../context.';
 import "./SearchForm.css";
 import SearchIcon from '@mui/icons-material/Search';
 
-const SearchForm = (props) => {
+const SearchForm = () => {
+  const {setSearchTerm, setResultTitle} = useContext();
+  const searchText = useRef('');
+  const navigate = useNavigate();
+
+  useEffect(() => searchText.current.focus(), []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let tempSearchTerm = searchText.current.value.trim();
+
+    if((tempSearchTerm.replace(/{^\s\$/g, '')).length === 0) {
+      /*if the search is empty, then the search is set to '0'*/
+      setSearchTerm("The Inferno");
+      setResultTitle("try another search");
+    }
+    else {
+      setSearchTerm(searchText.current.value);
+    }
+    navigate('/search');
+
   return (
     <div className= 'search-form'>
       <div className= 'container'>
@@ -22,6 +41,7 @@ const SearchForm = (props) => {
     </div>
   </div>
   )
+}
 }
 
 
