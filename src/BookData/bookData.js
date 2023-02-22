64a69a81db074b46bd2f48f0b490dee6027913f9
dante taylor <dantewanders@gmail.com>
2023-02-22 15:18:59 -0500

    Details page 2

--- src/BookData/bookData.js
+++ src/BookData/bookData.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useCallback } from "react";
 const URL = "http://openlibrary.org/search.json?title=";
 const BookContext = React.createContext();
--- src/components/BookDetails/BookDetails.js
+++ src/components/BookDetails/BookDetails.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import Loading from '../Loader/Loader'
+import coverImg from '../../images/cover_NF.jpg';
+import './BookDetails.css'
+import { useNavigate } from 'react-router-dom';
+
+const URL = "https://openlibrary.org/works/";
 
 const BookDetails = () =>{
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false)
+  const [book, setBook] = useState(null)
+  const navigate = useNavigate();
+  useEffect (() => {
+    setLoading(true)
+    async function getBookDetails(){
+      
+    }
   return (
     <div>BookDetails</div>
   )
